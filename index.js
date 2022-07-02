8f187eb47415bb421ea23e4c79c3d682a06f55ce
Brigita Martin <floofybat@brigitas-air.lan>
2022-07-01 22:32:37 -0600

    Got basic functionality in place. I LOVE YARGS

--- index.js
+++ index.js
@@ -3,36 +3,85 @@ import { parse } from "csv-parse/sync";
 import fs from "fs";
 import yargs from "yargs";
 import fetch from "node-fetch";
-import dotenv from 'dotenv/config';
+import dotenv from "dotenv/config";
+import { distance } from "./distance.js";
 
-var requestOptions = {
-  method: 'GET',
-};
+async function main() {
+  const input = fs.readFileSync(`./store-locations.csv`);
+  const locations = parse(input, {
+    columns: true,
+    skip_empty_lines: true,
+  });
+  const options = yargs
+    .usage("Usage: store-find <command> <options>")
+    .option("address", {
+      alias: "a",
+      describe: "Starting point in standard postal address format",
+      type: "string",
+    })
+    .option("zip", {
+      alias: "z",
+      describe: "The starting point as a six-digit zipcode",
+      type: "string",
+    })
+    .option("units", {
+      alias: "u",
+      describe:
+        "Which system of unit you would like your answer in; either kilometers or miles.",
+      type: "string",
+      default: "mi",
+      choices: ["km", "mi"],
+    })
+    .option("output", {
+      alias: "o",
+      describe:
+        "How you would like your output to look; either as text or in a json format.",
+      type: "string",
+      default: "text",
+      choices: ["text", "json"],
+    }).argv;
 
-const input = fs.readFileSync(`./store-locations.csv`);
-const locations = parse(input, {
-  columns: true,
-  skip_empty_lines: true
-});
+  const startingPoint = options.address || options.zip;
 
-console.log(locations[0]);
+  if (startingPoint) {
+    var result = await fetch(
+      `https://api.geoapify.com/v1/geocode/search?text=${startingPoint}&apiKey=${process.env.geoapify}`
+    )
+      .then((response) => response.json())
+      .catch((error) => console.log("error", error));
 
-const options = yargs
- .usage("Usage: store-find <command> <options>")
- .option("address", { alias: "address", describe: "starting point in standard postal address format", type: "string", demandOption: false })
- .option("zip", {alias: "zipcode", describe: "the starting point as a six-digit zipcode", type: "string", demandOption: false})
- .option("units", {alias: "distance format", describe: "which system of unit you would like your answer in", type: "string", demandOption: false})
- .option("output", {alias: "result format", describe: "either json or text file", type: "string", demandOption: false})
- .argv;
+    const { lat, lon } = result.features[0].properties;
 
-const startingPoint = options.address || options.zip;
+    if (lat && lon) {
+      locations.sort(
+        (startLoc, endLoc) =>
+          distance(lat, lon, startLoc.Latitude, startLoc.Longitude) -
+          distance(lat, lon, endLoc.Latitude, endLoc.Longitude)
+      );
+      var answer = {
+        storeAddress: locations[0].Address,
+        distance: distance(
+          lat,
+          lon,
+          locations[0].Latitude,
+          locations[0].Longitude,
+          options.units
+        ),
+      };
 
-if (startingPoint) {
-  fetch(`https://api.geoapify.com/v1/geocode/search?text=${startingPoint}&apiKey=${process.env.geoapify}`, requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+      if (options.output == "json") {
+        console.log(answer);
+      } else {
+        console.log(
+          `The nearest store address is ${
+            answer.storeAddress
+          }, and the distance is ${answer.distance.toFixed(2)} ${options.units}`
+        );
+      }
+    }
+  } else {
+    console.log("Please enter a complete address or a six-digit zipcode.");
+  }
 }
-else {
-  console.log("Please enter an address or zipcode.");
-}
+
+main();
