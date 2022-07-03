da2564ac4d5c01ae5856fa2616bf75d942b9a5a9
Brigita Martin <floofybat@brigitas-air.lan>
2022-07-02 23:25:30 -0600

    More refactoring.

--- src/callGeocodingAPI.js
+++ src/callGeocodingAPI.js
@@ -1,7 +1,19 @@
 import fetch from "node-fetch";
 
-export async function callGeocodingAPI(inputString) {
-  return await fetch(inputString)
+export async function callGeocodingAPI(options) {
+  const inputString = `https://api.geoapify.com/v1/geocode/search?text=${
+    options.address || options.zip
+  }&apiKey=${process.env.geoapify}`;
+  var result = await fetch(inputString)
     .then((response) => response.json())
     .catch((error) => console.log("error", error));
+  const { lat, lon } = result.features[0].properties;
+  // check if coordinates exist
+  if (!lat || !lon) {
+    console.log(
+      "No valid address or zipcode found. Please enter a valid address or zipcode."
+    );
+    process.exit();
+  }
+  return { lat, lon };
 }
--- src/index.js
+++ src/index.js
@@ -10,36 +10,29 @@ async function main() {
   // define cli options
   const options = defineCLIOptions();
   // ping geoapify api to see if a valid result returns
-  const inputForAPI = `https://api.geoapify.com/v1/geocode/search?text=${
-    options.address || options.zip
-  }&apiKey=${process.env.geoapify}`;
-  var result = await callGeocodingAPI(inputForAPI);
-  const { lat, lon } = result.features[0].properties;
-  // if the coordinates exist
-  if (lat && lon) {
-    // parse csv file into an array of arrays
-    const locations = readFile(`./src/store-locations.csv`);
-    // sort the locations array based on closest distance to the starting point
-    const loc = findClosestStore(locations, lat, lon);
-    // define the answer with the store address and distance in given units
-    var answer = {
-      storeAddress: `${loc.Address}, ${loc.City}, ${loc.State} ${loc["Zip Code"]}`,
-      distance: `${distance(
-        lat,
-        lon,
-        loc.Latitude,
-        loc.Longitude,
-        options.units
-      ).toFixed(2)} ${options.units}`,
-    };
-    // console log answer in either json or text format
-    if (options.output == "json") {
-      console.log(answer);
-    } else {
-      console.log(
-        `The nearest store address is ${answer.storeAddress}, and the distance is ${answer.distance}`
-      );
-    }
+  const { lat, lon } = await callGeocodingAPI(options);
+  //parse csv file into an array of arrays
+  const locations = readFile(`./src/store-locations.csv`);
+  // sort the locations array based on closest distance to the starting point
+  const loc = findClosestStore(locations, lat, lon);
+  // define the answer with the store address and distance in given units
+  var answer = {
+    storeAddress: `${loc.Address}, ${loc.City}, ${loc.State} ${loc["Zip Code"]}`,
+    distance: `${distance(
+      lat,
+      lon,
+      loc.Latitude,
+      loc.Longitude,
+      options.units
+    ).toFixed(2)} ${options.units}`,
+  };
+  // console log answer in either json or text format
+  if (options.output == "json") {
+    console.log(answer);
+  } else {
+    console.log(
+      `The nearest store address is ${answer.storeAddress}, and the distance is ${answer.distance}`
+    );
   }
 }
 
